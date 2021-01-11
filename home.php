b5cc37fc30871d2335c68e027ffe65137b67c008
mariagroza <72649463+mariagroza@users.noreply.github.com>
2021-01-12 01:27:14 +0200

    Add files via upload

--- home.php
+++ home.php
@@ -1,5 +1,20 @@
 <?php get_header(); 
 ?>
 
+<!-- hero -->
+
+<div class="main-page">
+
+	<div class="content-container">
+		<!-- post 1 -->
+		<!-- post 2 -->
+		<!-- post 3 -->
+		<!-- sidebar -->
+		<!-- pagination -->
+	</div>
+		
+</div>
+
+
 <?php get_footer();
 ?>
