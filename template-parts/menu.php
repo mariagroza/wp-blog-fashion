87d9bf2e055f35152d3b8dba345e7eeb6be7ede6
mariagroza <72649463+mariagroza@users.noreply.github.com>
2021-01-12 19:07:25 +0200

    Add files via upload

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -1,7 +1,7 @@
-<ul class="navbar-nav">
-	<li class="nav-item active"><a class="nav-link">Home</a></li>
-	<li class="nav-item"><a class="nav-link">Recipes</a></li>
-	<li class="nav-item"><a class="nav-link">Article</a></li>
-	<li class="nav-item"><a class="nav-link">Contact</a></li>
-	<li class="nav-item"><a class="nav-link">Purchase</a></li>
+<ul>
+	<li>Home</li>
+	<li>Recipes</li>
+	<li>Article</li>
+	<li>Contact</li>
+	<li>Purchase</li>
 </ul>	
