93230eccbc644f9438032705fd1a4a158abf0fc1
mariagroza <72649463+mariagroza@users.noreply.github.com>
2021-02-23 00:02:17 +0200

    Add files via upload

---
+++ template-parts/social-media-icons-footer.php
@@ -0,0 +1,12 @@
+<div class="footer-social-media-icons">
+	<a href="#" class="facebook-icon"><div></div></a>
+	<a href="#" class="twitter-icon"><div></div></a>
+	<a href="#" class="pinteret-icon"><div></div></a>
+	<a href="#" class="google-icon"><div></div></a>
+	<a href="#" class="behance-icon"><div></div></a>
+	<a href="#" class="instagram-icon"><div></div></a>
+</div>
+
+<!-- <?php if (is_active_sidebar('social-media-icons')) :
+    dynamic_sidebar('social-media-icons'); ?>
+<?php endif; ?> -->
