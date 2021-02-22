5290749b5782afa530c29a27a2cbdfddd3e8c133
mariagroza <72649463+mariagroza@users.noreply.github.com>
2021-02-22 23:55:42 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Fashion Blog Template</title>
 
-	 <?php 
+	<?php 
 	wp_head();
 	?>
 
@@ -11,7 +13,11 @@
 
 <body>
 
-	<header> 
-		<?php get_template_part( 'template-parts/logotext', 'logotext' ); ?>
-		<?php get_template_part( 'template-parts/menu', 'menu' ); ?>
+	<header class="header"> 
+		<?php get_template_part( 'template-parts/logo'); ?>
+		<?php wp_nav_menu(
+			array(
+				'theme_location' => 'top-menu',
+			)
+		);?>
 	</header>
